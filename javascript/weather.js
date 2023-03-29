8e06ab4ab8105ce73bf51dfdc01745719a0fa14a
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-03-29 10:40:42 -0400

    Weather works

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,5 +1,39 @@
 let daysSelected = 7;
 
+getWeekdays()
+requestJQuery()
+
+function getWeekdays()
+{
+    let today = new Date();
+    document.querySelector('.day[data-day="0"]').innerHTML = dayToWord(today.getDay());
+    document.querySelector('.day[data-day="1"]').innerHTML = dayToWord((today.getDay() + 1) % 7);
+    document.querySelector('.day[data-day="2"]').innerHTML = dayToWord((today.getDay() + 2) % 7);
+    document.querySelector('.day[data-day="3"]').innerHTML = dayToWord((today.getDay() + 3) % 7);
+    document.querySelector('.day[data-day="4"]').innerHTML = dayToWord((today.getDay() + 4) % 7);
+    document.querySelector('.day[data-day="5"]').innerHTML = dayToWord((today.getDay() + 5) % 7);
+    document.querySelector('.day[data-day="6"]').innerHTML = dayToWord((today.getDay() + 6) % 7);
+}
+
+function dayToWord(num)
+{
+    switch (num) {
+        case 0:
+            return 'Sunday'
+        case 1:
+            return 'Monday'
+        case 2:
+            return 'Tuesday'
+        case 3:
+            return 'Wednesday'
+        case 4:
+            return 'Thursday'
+        case 5:
+            return 'Friday'
+        case 6:
+            return 'Saturday'
+    }
+}
 function requestJQuery() {
     $.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/80425/next7days?unitGroup=metric&key=GTAYYWQQLFFS6VE7UZR9YUMKJ&contentType=json') .done(function(rawResponse) {
         processWeatherData(rawResponse);
@@ -13,7 +47,7 @@ function requestJQuery() {
 
      const date = new Date();
      const laterDate = new Date();
-     laterDate.setDate(date.getDate() + daysSelected);
+     laterDate.setDate(date.getDate() + 7);
 
      let month = date.getMonth() + 1;
      if (month < 10) month = '0' + month;
@@ -44,5 +78,10 @@ function processWeatherData(response) {
 
     for (var i=0;i<daysSelected;i++) {
         console.log(days[i].datetime+": tempmax="+days[i].tempmax+", tempmin="+days[i].tempmin);
+        
+        document.querySelector('.weather-icon[data-day="' + i + '"]').innerHTML = '<img src="assets/weather/' + days[i].icon + '.png">'
+        document.querySelector('.low-temp[data-day="' + i + '"]').innerHTML = "Lowest Temp: " + days[i].tempmin
+        document.querySelector('.high-temp[data-day="' + i + '"]').innerHTML = "Highest Temp: " + days[i].tempmax
+
     }
 }
